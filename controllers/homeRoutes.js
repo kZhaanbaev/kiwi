8feaf56ba93e85f6d088097cc1f63472156e130c
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-05-22 13:36:34 -0400

    get route updated for homepage and login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,48 +3,48 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    //add code to direct to homepage
+    res.render('homepage');
     
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/departments/', async (req, res) => {
-  try {
-        //add code to direct to departments page
+// router.get('/departments/', async (req, res) => {
+//   try {
+//         //add code to direct to departments page
 
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.get('/departments/departmentName', async (req, res) => {
-    try {
-          //add code to direct to a specific department. This page will deisplay every product belonging to a department
+// router.get('/departments/departmentName', async (req, res) => {
+//     try {
+//           //add code to direct to a specific department. This page will deisplay every product belonging to a department
 
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
-router.get('/cart', withAuth, async (req, res) => {
-  try {
-        //add code to direct to cart page
+// router.get('/cart', withAuth, async (req, res) => {
+//   try {
+//         //add code to direct to cart page
 
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
    //add code to direct to login page
 
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect('/profile');
+  //   return;
+  // }
 
   res.render('login');
 });
