90569dbde4ad4a688ad116146c10778159604a08
Stephanie <whatawhat@hotmail.com>
2021-05-22 12:47:50 -0400

    added rendering for cart and departments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,40 +11,43 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/departments/', async (req, res) => {
-  try {
-        //add code to direct to departments page
+  res.render('departments');
+  // try {
+  //       //add code to direct to departments page
 
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
-router.get('/departments/departmentName', async (req, res) => {
-    try {
-          //add code to direct to a specific department. This page will deisplay every product belonging to a department
+// router.get('/departments/departmentName', async (req, res) => {
+//     try {
+//           //add code to direct to a specific department. This page will deisplay every product belonging to a department
 
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
-router.get('/cart', withAuth, async (req, res) => {
-  try {
-        //add code to direct to cart page
+//router.get('/cart', withAuth, async (req, res) => {
+router.get('/cart', (req, res) => {
+res.render('cart');
+  // try {
+  //       //add code to direct to cart page
 
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 router.get('/login', (req, res) => {
-   //add code to direct to login page
+  //  //add code to direct to login page
 
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+  // // If the user is already logged in, redirect the request to another route
+  // if (req.session.logged_in) {
+  //   res.redirect('/profile');
+  //   return;
+  // }
 
   res.render('login');
 });
