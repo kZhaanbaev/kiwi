3c366fa2719ae185e4315f859679360e8b8291ec
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-05-27 16:15:36 -0400

    changed models, still in progress

---
+++ models/DepartmentItem.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class DepartmentItem extends Model{ }
+
+DepartmentItem.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true  
+        },
+        item_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'item',
+                key: 'item_id',
+                unique: false
+            }
+        },
+        department_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'department',
+                key: 'department_id',
+                unique: false
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'department_item'
+    }
+);
+
+module.exports = DepartmentItem;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const Buyer = require('./Buyer');
 const Item = require('./Item');
 const Department = require('./Department');
+const DepartmentItem = require('./DepartmentItem');
+const sequelize = require('../config/connection');
+const { Sequelize } = require('sequelize/types');
 
 Buyer.hasMany(Item, {
     foreignKey: 'buyer_id'
@@ -10,12 +13,25 @@ Item.belongsTo(Buyer, {
     foreignKey: 'buyer_id'
 });
 
-Department.hasMany(Item, {
+// Department.hasMany(Item, {
+//     foreignKey: 'department_id'
+// });
+
+// Item.belongsTo(Buyer, {
+//     foreignKey: 'buyer_id'
+// });
+
+Department.belongsToMany(Item, {
+    through: DepartmentItem,
+    // as: 'items',
     foreignKey: 'department_id'
 });
 
-Item.belongsTo(Department, {
-    foreignKey: 'department_id'
+
+Item.belongsToMany(Department, {
+    through: DepartmentItem,
+    // as: 'departments',
+    foreignKey: 'item_id'
 });
 
 module.exports = {Buyer, Item, Department};
